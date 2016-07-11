dbdf774a85efb756347695a02db42073300b877f
Paul <paulzocker21@googlemail.com>
2016-07-11 18:42:00 +0200

    Grundgerüst

---
+++ Uplink/src/us/tim/paul/Main.java
@@ -0,0 +1,24 @@
+package us.tim.paul;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	@Override
+	public void onEnable() {
+
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		return false;
+	}
+	
+}
